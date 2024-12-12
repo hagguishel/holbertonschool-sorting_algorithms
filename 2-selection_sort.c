a19b600cd967b16abfe6b416d3be872a28f0db1b
Zairth <mano.gimenez@hotmail.com>
2024-12-12 16:37:54 +0100

    Adding task-2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,7 +1,7 @@
 #include "sort.h"
 
 /**
- * selection_sort - sorts an array of integers in ascending order using the Selection sort algorithm
+ * selection_sort - sorts an array using the Selection sort algorithm
  *
  * @array: the array to sort
  * @size: the size of the array
@@ -11,26 +11,29 @@ void selection_sort(int *array, size_t size)
 {
 	size_t i = 0;
 	size_t j = 0;
+	int mini;
+	int mini_index;
 	int temp;
-	int tmp;
 
 	if (array == NULL || size < 2)
 		return;
 
-	while (i < size)
+	while (i < size - 1)
 	{
 		j = i + 1;
+		mini = array[i];
 		while (j < size)
 		{
-			if (array[j] < array[j + 1] && array[j + 1] != 0)
+			if (array[j] < mini)
 			{
-				temp = j;
+				mini = array[j];
+				mini_index = j;
 			}
 			j++;
 		}
-		tmp = array[i];
-		array[i] = array[temp];
-		array[temp] = tmp;
+		temp = array[i];
+		array[i] = array[mini_index];
+		array[mini_index] = temp;
 		print_array(array, size);
 		i++;
 	}
