b0ad8842d2c330abec0b858c918af115786cc15f
Zairth <mano.gimenez@hotmail.com>
2024-12-12 13:59:29 +0100

    Resolving betty problem for the task-1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -7,36 +7,38 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *current, *temp ,*prev;
+	listint_t *current;
+	listint_t *temp;
+	listint_t *prev;
 
-    if (!list || !*list || !(*list)->next)
-        return;
+	if (!list || !*list || !(*list)->next)
+		return;
 
-    current = (*list)->next;
-    while (current)
-    {
-        prev = current->prev;
-        while (prev && current->n < prev->n)
-        {
-            temp = current->next;
-            if (prev->prev)
-                prev->prev->next = current;
-            current->prev = prev->prev;
-            
-            prev->next = temp;
-            if (temp)
-                temp->prev = prev;
-            
-            current->next = prev;
-            prev->prev = current;
-            
-            if (!current->prev)
-                *list = current;
+	current = (*list)->next;
+	while (current)
+	{
+		prev = current->prev;
+		while (prev && current->n < prev->n)
+		{
+			temp = current->next;
+			if (prev->prev)
+				prev->prev->next = current;
+			current->prev = prev->prev;
 
-            print_list(*list);
+			prev->next = temp;
+			if (temp)
+				temp->prev = prev;
 
-            prev = current->prev;
-        }
-        current = current->next;
-    }
+			current->next = prev;
+			prev->prev = current;
+
+			if (!current->prev)
+				*list = current;
+
+			print_list(*list);
+
+			prev = current->prev;
+		}
+		current = current->next;
+	}
 }
