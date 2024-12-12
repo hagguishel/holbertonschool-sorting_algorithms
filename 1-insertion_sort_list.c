88fe4124f6365fba777771007e4340baaad2f1ab
hagguishel <hagguirzt@gmail.com>
2024-12-12 13:42:43 +0100

    add exo 1

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,42 @@
+#include "sort.h"
+/**
+ * insertion_sort_list - Trie une liste doublement chaînée d'entiers en
+ * ordre croissant en utilisant l'algorithme de tri par insertion.
+ *
+ * @list: Pointeur vers la tête de la liste.
+ */
+void insertion_sort_list(listint_t **list)
+{
+    listint_t *current, *temp ,*prev;
+
+    if (!list || !*list || !(*list)->next)
+        return;
+
+    current = (*list)->next;
+    while (current)
+    {
+        prev = current->prev;
+        while (prev && current->n < prev->n)
+        {
+            temp = current->next;
+            if (prev->prev)
+                prev->prev->next = current;
+            current->prev = prev->prev;
+            
+            prev->next = temp;
+            if (temp)
+                temp->prev = prev;
+            
+            current->next = prev;
+            prev->prev = current;
+            
+            if (!current->prev)
+                *list = current;
+
+            print_list(*list);
+
+            prev = current->prev;
+        }
+        current = current->next;
+    }
+}
