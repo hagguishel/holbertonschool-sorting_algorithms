bb95dd30324903250f89fdd51543657a55170289
Zairth <mano.gimenez@hotmail.com>
2024-12-12 15:40:30 +0100

    Adding the beggining of the task-2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -9,5 +9,29 @@
  */
 void selection_sort(int *array, size_t size)
 {
+	size_t i = 0;
+	size_t j = 0;
+	int temp;
+	int tmp;
 
+	if (array == NULL || size < 2)
+		return;
+
+	while (i < size)
+	{
+		j = i + 1;
+		while (j < size)
+		{
+			if (array[j] < array[j + 1] && array[j + 1] != 0)
+			{
+				temp = j;
+			}
+			j++;
+		}
+		tmp = array[i];
+		array[i] = array[temp];
+		array[temp] = tmp;
+		print_array(array, size);
+		i++;
+	}
 }
