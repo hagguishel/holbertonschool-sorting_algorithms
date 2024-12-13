a84f8faa6150b4e9a46843d77df0a0dc5c336846
Zairth <mano.gimenez@hotmail.com>
2024-12-13 14:40:09 +0100

    Correction of the betty style for the task-3

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -11,32 +11,34 @@
  */
 int lomuto_partition(int *array, int low, int high, size_t size)
 {
-    int pivot = array[high];
-    int i = low - 1;
-    int j;
+	int pivot = array[high];
+	int i = low - 1;
+	int j;
 
-    for (j = low; j < high; j++)
-    {
-        if (array[j] < pivot)
-        {
-            i++;
-            if (i != j)
-            {
-                int temp = array[i];
-                array[i] = array[j];
-                array[j] = temp;
-                print_array(array, size);
-            }
-        }
-    }
-    if (array[i + 1] != array[high])
-    {
-        int temp = array[i + 1];
-        array[i + 1] = array[high];
-        array[high] = temp;
-        print_array(array, size);
-    }
-    return (i + 1);
+	for (j = low; j < high; j++)
+	{
+		if (array[j] < pivot)
+		{
+			i++;
+			if (i != j)
+			{
+				int temp = array[i];
+
+				array[i] = array[j];
+				array[j] = temp;
+				print_array(array, size);
+			}
+		}
+	}
+	if (array[i + 1] != array[high])
+	{
+		int temp = array[i + 1];
+
+		array[i + 1] = array[high];
+		array[high] = temp;
+		print_array(array, size);
+	}
+	return (i + 1);
 }
 
 /**
@@ -48,13 +50,13 @@ int lomuto_partition(int *array, int low, int high, size_t size)
  */
 void quick_sort_helper(int *array, int low, int high, size_t size)
 {
-    if (low < high)
-    {
-        int pivot_index = lomuto_partition(array, low, high, size);
+	if (low < high)
+	{
+		int pivot_index = lomuto_partition(array, low, high, size);
 
-        quick_sort_helper(array, low, pivot_index - 1, size);
-        quick_sort_helper(array, pivot_index + 1, high, size);
-    }
+		quick_sort_helper(array, low, pivot_index - 1, size);
+		quick_sort_helper(array, pivot_index + 1, high, size);
+	}
 }
 
 /**
@@ -64,8 +66,8 @@ void quick_sort_helper(int *array, int low, int high, size_t size)
  */
 void quick_sort(int *array, size_t size)
 {
-    if (!array || size < 2)
-        return;
+	if (!array || size < 2)
+		return;
 
-    quick_sort_helper(array, 0, size - 1, size);
+	quick_sort_helper(array, 0, size - 1, size);
 }
